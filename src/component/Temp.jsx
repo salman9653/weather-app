d7fd40a34931d39b45460833214b10b3500a49e2
Salman <salman9915189734@gmail.com>
2022-03-17 21:21:48 +0530

    api added

--- src/component/Temp.jsx
+++ src/component/Temp.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Temp = () => {
+    const api_key = "89594c44132fba6bc4d4697545bb9efd"
+    const [searchCity, setSearchCity] = useState("delhi");
+    const [tempInfo, setTempInfo] = useState({});
+    const getWeatherInfo = async () => {
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=metric&appid=${api_key}`;
+            const res = await fetch(url);
+            const data = await res.json();
+
+            const { temp, humidity, pressure } = data.main;
+            const { main: weatherMood } = data.weather[0];
+            const { name } = data;
+            const { speed } = data.wind;
+            const { country, sunset } = data.sys
+            const myWeatherInfo = { temp, humidity, pressure, weatherMood, name, speed, country, sunset };
+
+            setTempInfo(myWeatherInfo);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getWeatherInfo();
+    }, [])
     return (
         <>
             <div className='wrap'>
@@ -10,9 +35,11 @@ const Temp = () => {
                         autoFocus
                         id='search'
                         className='searchTerm'
+                        value={searchCity}
+                        onChange={(e) => { setSearchCity(e.target.value) }}
                         placeholder='search...'
                     />
-                    <button className="searchButton">Search</button>
+                    <button className="searchButton" onClick={getWeatherInfo}>Search</button>
                 </div>
             </div>
             <section className="widget">
@@ -21,7 +48,7 @@ const Temp = () => {
                 </div>
                 <div className="weatherInfo">
                     <div className="temperature">
-                        <span>25 &deg;</span>
+                        <span> &deg;</span>
                     </div>
                     <div className="description">
                         <div className="weatherCondition">
